0920b993422989af37524d4e83bc9c0789f20873
dafnelp <dafnelozano88@hotmail.com>
2019-01-29 16:12:12 +0100

    Program for count the number of bases in a DNA sequence from a file.

---
+++ P0/dna_count_file.py
@@ -0,0 +1,19 @@
+with open('dna.txt', 'r') as f:
+
+    for line in f:
+
+        line = line.replace('\n', '')
+
+        length = len(line)
+
+        count_a = line.count('A')
+        count_c = line.count('C')
+        count_t = line.count('T')
+        count_g = line.count('G')
+
+        print(line)
+        print('Total number of bases for each line:', length)
+        print('A:', count_a)
+        print('C:', count_c)
+        print('T:', count_t)
+        print('G:', count_g)
