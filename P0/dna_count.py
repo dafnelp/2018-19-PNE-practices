0d1174a287f67d30bc4956d54f6c02c3ccdc9179
dafnelp <dafnelozano88@hotmail.com>
2019-01-29 16:05:02 +0100

    Program for count the number of bases in a DNA sequence.

---
+++ P0/dna_count.py
@@ -0,0 +1,15 @@
+dna_seq = 'CATGTAGACTAG'
+
+length_dna = len(dna_seq)
+
+print('Total length:', length_dna)
+
+count_a = dna_seq.count('A')
+count_c = dna_seq.count('C')
+count_t = dna_seq.count('T')
+count_g = dna_seq.count('G')
+
+print('A:', count_a)
+print('C:', count_c)
+print('T:', count_t)
+print('G:', count_g)
