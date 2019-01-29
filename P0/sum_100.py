e4352649ac41380091201b14ae01ab28a1a587b2
dafnelp <dafnelozano88@hotmail.com>
2019-01-29 15:44:18 +0100

    Adding the numbers from 1 to 100.

---
+++ P0/sum_100.py
@@ -0,0 +1,8 @@
+print('Program for the sum of the first 100 integer numbers')
+
+total_sum = 0
+
+for i in range(101):
+    total_sum += i
+
+print('The result of the sum of the first 100 numbers is:', total_sum)
