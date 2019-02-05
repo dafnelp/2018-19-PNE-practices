d3a97e56ae968d67611f7a73631102fa296ca7d8
dafnelp <dafnelozano88@hotmail.com>
2019-02-05 16:45:03 +0100

    Program for different class

---
+++ session-5/Seq1.py
@@ -0,0 +1,30 @@
+class Seq:
+    """ A class for representing sequences"""
+    def __init__(self, strbases):
+        print("New sequence created!")
+
+        self.strbases = strbases
+
+    def len(self):
+        return len(self.strbases)
+
+
+class Gene(Seq):
+    """" This class is derived from the Seq
+    All the objects of class Gene will
+    inheritage the methods from Seq Class"""
+    pass
+
+s1 = Gene("ATTCGATCC")
+s2 = Seq("AAACGG")
+
+str1 = s1.strbases
+str2 = s2.strbases
+
+l1 = s1.len()
+l2 = s2.len()
+
+print("Sequence 1: ", str1, "\n length:", l1)
+print("Sequence 2: ", str2, "\n length:", l2)
+
+print("The end")
