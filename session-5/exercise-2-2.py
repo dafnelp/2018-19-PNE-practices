252879b1f1bb702dd04a60e200bfe4cd005acc59
dafnelp <dafnelozano88@hotmail.com>
2019-02-07 12:16:27 +0100

    Program with the main function of the exercise 2 that call the function from the Bases file

---
+++ session-5/exercise-2-2.py
@@ -0,0 +1,13 @@
+from Bases import count_bases
+
+# Main program
+
+s1 = input("Please enter the first sequence: ")
+s2 = input("Please enter the second sequence:")
+
+n_base1 = count_bases(s1)
+n_base2 = count_bases(s2)
+
+print(n_base1)
+print()
+print(n_base2)
