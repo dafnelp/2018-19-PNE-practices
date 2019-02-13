d2293a3a4c596d9245b0c5cb0596622f8d8fb944
dafnelp <dafnelozano88@hotmail.com>
2019-02-13 18:48:08 +0100

    Practice 2 that use a program to connect to the server and give the complement and reverse of some sequences

---
+++ P2/Practice-2.py
@@ -0,0 +1,60 @@
+import socket
+
+
+class Seq:
+
+    def __init__(self, strbases):
+
+        self.bases = strbases
+
+    def complement(self):
+
+        base_complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+        c_seq = []
+
+        for character in self.bases:
+            c_seq.append(base_complement[character])
+
+        return ''.join(c_seq)
+
+    def reverse(self):
+        r_seq = []
+        len_seq = len(self.bases)
+
+        for i in self.bases:
+            len_seq = len_seq - 1
+            r_seq.append(self.bases[len_seq])
+
+        return ''. join(r_seq)
+
+
+# Connect to the server using a while loop for asking to different sequences
+
+s_msg = True
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("Socket created")
+
+    PORT = 8083
+    IP = "212.128.253.75"
+
+    s.connect((IP, PORT))
+
+    str_msg = input("Please, enter a sequence of amino acids: ")
+    str_msg = Seq(str_msg)
+
+    seq_comp = str_msg.complement()
+    seq_rev = str_msg.reverse()
+
+    s.send(str.encode(seq_comp))
+    s.send(str.encode(seq_rev))
+
+    msg = s.recv(2048).decode("utf-8")
+    print("MESSAGE FROM THE SERVER:")
+    print(msg)
+
+    s.close()
+
+    print("The end")
