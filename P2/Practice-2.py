2616007b68e5c53e187381304585cc1582805a21
dafnelp <dafnelozano88@hotmail.com>
2019-02-19 21:41:06 +0100

    Practice 2. Program that send a sequence of amino acids to the server and the server return the complement

--- P2/Practice-2.py
+++ P2/Practice-2.py
@@ -1,53 +1,24 @@
 import socket
 
-
-class Seq:
-
-    def __init__(self, strbases):
-
-        self.bases = strbases
-
-    def complement(self):
-
-        base_complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-        c_seq = []
-
-        for character in self.bases:
-            c_seq.append(base_complement[character])
-
-        return ''.join(c_seq)
-
-    def reverse(self):
-        r_seq = []
-        len_seq = len(self.bases)
-
-        for i in self.bases:
-            len_seq = len_seq - 1
-            r_seq.append(self.bases[len_seq])
-
-        return ''. join(r_seq)
-
+from Seq import Seq
 
 # Connect to the server using a while loop for asking for different sequences
+PORT = 8082
+IP = "10.0.2.15"
 
 s_msg = True
 
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    print("Socket created")
-
-    PORT = 8082
-    IP = "212.128.253.64"
-
-    s.connect((IP, PORT))
-
     str_msg = input("Please, enter a sequence of amino acids: ")
     str_msg = Seq(str_msg)
 
     seq_comp = str_msg.complement()
     seq_comp = Seq(seq_comp)
     seq_rev = seq_comp.reverse()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect((IP, PORT))
 
     s.send(str.encode(seq_rev))
 
