e3f3b935ccf6351729ce4bee13ab93b526227afc
dafnelp <dafnelozano88@hotmail.com>
2019-03-06 18:30:02 +0100

    Web server using the HTTP python module that opens two different files depending the client request

--- session-13/webserver1.py
+++ session-13/webserver1.py
@@ -1,44 +1,83 @@
 import http.server
 import socketserver
-
+# Server's port
 PORT = 8001
 
 
+# Class with our handler
 class TestHandler(http.server.BaseHTTPRequestHandler):
+
     def do_GET(self):
+        """ Method that is called whenever the GET method is invoked
+        in the HTTP protocol request"""
+
         print("GET received")
 
+        # Print the request line of the client
         print("Request line:" + self.requestline)
+
+        # Print the command received
         print(" Cmd: " + self.command)
+
+        # Print the resource received (the path)
         print(" Path: " + self.path)
 
+        # Path requested by the client
         if self.path == "/":
+
+            # Open the file
             with open("index.html", "r") as f:
                 content = f.read()
 
-            self.send_response(200)
+            # Generating the response message
+            self.send_response(200)  # --- status line - everything Ok
+
+            # Define the content-type header:
             self.send_header('Content-Type', 'text/html')
             self.send_header('Content-Length', len(str.encode(content)))
+
+            # The header is finished
             self.end_headers()
 
+            # Send the response message
             self.wfile.write(str.encode(content))
+
         else:
+            # Open the file
             with open("error.html", "r") as f:
                 content = f.read()
-            self.send_response(200)
+            # Generating the response message
+            self.send_response(404)  # --- status line - file not found error
+
+            # Define the content-type header:
             self.send_header('Content-Type', 'text/html')
             self.send_header('Content-Length', len(str.encode(content)))
+
+            # The header is finished
             self.end_headers()
 
+            # Send the response message
             self.wfile.write(str.encode(content))
 
         return
 
+# MAIN PROGRAM
 
+# Set the new handler
 Handler = TestHandler
 
-# LoCAL IP address
+# Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving at PORT", PORT)
 
-    httpd.serve_forever()
+    # Main loop: Attend the client. Whenever there is a new
+    # client, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
