ca84404ed73356fef2a10b62e25a78b12a68d6b5
dafnelp <dafnelozano88@hotmail.com>
2019-03-22 22:33:07 +0100

    Class Seq

--- P1/Seq.py
+++ P1/Seq.py
@@ -1,46 +1,63 @@
 class Seq:
     def __init__(self, strbases):
 
-        self.bases = strbases
+        self.strbases = strbases
 
     def len(self):
 
-        return len(self.bases)
+        return len(self.strbases)
 
-    def count(self):
-
-        count_a = "A: {}".format(self.bases.count('A'))
-        count_c = "C: {}".format(self.bases.count('C'))
-        count_t = "T: {}".format(self.bases.count('T'))
-        count_g = "G: {}".format(self.bases.count('G'))
-
-        return count_a, count_c, count_t, count_g
+    def count_bases(self, base):
+        if base == "A":
+            a_base = self.strbases.count("A")
+            count_a = "A: {}".format(a_base)
+            return count_a
+        elif base == "C":
+            c_base = self.strbases.count("C")
+            count_c = "C: {}".format(c_base)
+            return count_c
+        elif base == "T":
+            t_base = self.strbases.count("T")
+            count_t = "T: {}".format(t_base)
+            return count_t
+        elif base == "G":
+            g_base = self.strbases.count("G")
+            count_g = "G: {}".format(g_base)
+            return count_g
 
     def complement(self):
 
         base_complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
         c_seq = []
 
-        for character in self.bases:
+        for character in self.strbases:
             c_seq.append(base_complement[character])
 
         return ''.join(c_seq)
 
     def reverse(self):
         r_seq = []
-        len_seq = len(self.bases)
+        len_seq = len(self.strbases)
 
-        for i in self.bases:
+        for i in self.strbases:
             len_seq = len_seq - 1
-            r_seq.append(self.bases[len_seq])
+            r_seq.append(self.strbases[len_seq])
 
         return ''. join(r_seq)
 
-    def perc(self):
+    def perc(self, base ):
+        if base == "A":
+            perc_a = "A: {} %".format(round(100.0 * self.strbases.count('A') / len(self.strbases), 1))
+            return perc_a
 
-        perc_a = "A: {} %".format(round(100.0 * self.bases.count('A') / len(self.bases), 1))
-        perc_c = "C: {} %".format(round(100.0 * self.bases.count('C') / len(self.bases), 1))
-        perc_t = "T: {} %".format(round(100.0 * self.bases.count('T') / len(self.bases), 1))
-        perc_g = "G: {} %".format(round(100.0 * self.bases.count('G') / len(self.bases), 1))
+        elif base == "C":
+            perc_c = "C: {} %".format(round(100.0 * self.strbases.count('C') / len(self.strbases), 1))
+            return perc_c
 
-        return perc_a, perc_c, perc_t, perc_g
+        elif base == "T":
+            perc_t = "T: {} %".format(round(100.0 * self.strbases.count('T') / len(self.strbases), 1))
+            return perc_t
+
+        elif base == "G":
+            perc_g = "G: {} %".format(round(100.0 * self.strbases.count('G') / len(self.strbases), 1))
+            return perc_g
