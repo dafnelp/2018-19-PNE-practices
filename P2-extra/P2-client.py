62a4d9156e1f883839290391337b609bc31570da
dafnelp <dafnelozano88@hotmail.com>
2019-04-02 22:05:41 +0200

    Practice 2 extra --- client

--- P2-extra/P2-client.py
+++ P2-extra/P2-client.py
@@ -6,7 +6,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("Socket created")
 
-PORT = 8083
+PORT = 8082
 IP = "10.0.2.15"
 
 # Connect to the server
@@ -21,9 +21,11 @@ client_msg = input("Enter a sequence of amino-acids: ")
 
 s.send(str.encode(client_msg))
 
+# Receiving the message
 msg = s.recv(2048).decode("utf-8")
 
-print("MESSAGE FROM THE SERVER:")
+# Printing the message
+print("Complementary of the sequence introduced:")
 print(msg)
 
 s.close()
